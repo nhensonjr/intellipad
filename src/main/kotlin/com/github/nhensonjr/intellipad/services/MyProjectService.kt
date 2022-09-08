48dd607d158cffaf5f4f1fd4a9ef9c47de7db5f5
GitHub Action <action@github.com>
2022-09-08 16:15:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/nhensonjr/intellipad/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nhensonjr/intellipad/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.nhensonjr.intellipad.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nhensonjr.intellipad.MyBundle
 
 class MyProjectService(project: Project) {
 
